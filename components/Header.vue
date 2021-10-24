f003c81d678c47a31e117d8ed046adb7d8779d4a
Tudor Tocan <tudor.tocan@gmail.com>
2021-10-24 02:24:15 +0100

    create fixed header

---
+++ components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="bg-white fixed inset-x-0 rounded-b-2xl shadow-md z-20">
+    <div class="bg-gradient-to-r from-green-600 to-blue-700 h-2"></div>
+    <div class="flex justify-end rounded-b-md rounded-b-sm my-4">
+      <a href="#" class="p-2 m-2 text-green-600 text-xl">Login</a>
+    </div>
+  </header>
+</template>
