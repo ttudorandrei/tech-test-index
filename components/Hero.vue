d062be343ab8de67b20a9a29712e58b314a2a9aa
Tudor Tocan <tudor.tocan@gmail.com>
2021-10-24 02:24:45 +0100

    create hero section w/ image

---
+++ components/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-full" style="background-color: #e9f9fd">
+    <h1 class="grid justify-items-center">
+      <div class="text-black text-5xl text-center pt-10 pb-3 w-2/3">
+        Deine Job website
+      </div>
+    </h1>
+    <img
+      class=""
+      src="../assets/img/handshake.PNG"
+      width="100%"
+      height="100%"
+    />
+  </div>
+</template>
